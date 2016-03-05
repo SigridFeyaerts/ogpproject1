f9c2664679d659f549b42294bdb4b2462a66bf7c
Eleanor2016 <Eleanor2016@users.noreply.github.com>
2016-03-05 12:59:36 +0100

    Update Unit.java

--- Unit.java
+++ Unit.java
@@ -1,5 +1,9 @@
 package hillbillies.model;
+
 import be.kuleuven.cs.som.annotate.*;
+
+import java.util.Arrays.*;
+
 import ogp.framework.util.ModelException;
 
 /**
@@ -9,38 +13,54 @@ import ogp.framework.util.ModelException;
  * @version 1.0
  * @author Sigrid Feyaerts, Eleanor Van Looy
  */
+ 
 public class Unit {
 	private String name;
 	private int strength,weight,agility,toughness,lc = 1;
-	private double[] position,targetposition,v,endTargetPosition;
-	private double orientation,vw,vb,vs,currentSpeed,distanceToGo,hitpoints,staminaPoints,workTime,restTime,minRestTime;
+	private double attackTime,orientation,vw,vb,vs,currentSpeed,distanceToGo,hitpoints,staminaPoints,workTime,restTime,minRestTime;
+	private double[] position,targetPosition,v,endTargetPosition;
+	private boolean enableDefaultBehaviour, isAttacking=false,isMoving=false,isSprinting=false,isWorking=false,isResting=true,inMinRestTime;;
 
-
+	private static final char [] validChars = new char[]{' ','\"','\''};
+	
 	/**
 	 * Initialize this new unit with given name, position, weight, strength, 
 	 * agility, toughness and state of default behaviour. 
 	 * 
 	 * @param 	name
-	 * 			The name of the unit.
+	 * 		The name of the unit.
 	 * @param 	initialPosition
-	 * 			The initial position of the unit, as an array with 3 elements
-	 *          {x, y, z}.
+	 * 		The initial position of the unit, as an array with 3 elements
+	 *          	{x, y, z}.
 	 * @param 	weight
-	 * 			The initial weight of the unit.
+	 * 		The initial weight of the unit.
 	 * @param 	agility
-	 * 			The initial agility of the unit.
+	 * 		The initial agility of the unit.
 	 * @param 	strength
-	 * 			The initial strength of the unit.
+	 * 		The initial strength of the unit.
 	 * @param 	toughness
-	 * 			The initial toughness of the unit.
+	 * 		The initial toughness of the unit.
 	 * @param 	enableDefaultBehaviour
-	 * 			Whether the default behaviour of the unit is enabled.
-	 * @post	
+	 * 		Whether the default behaviour of the unit is enabled.
+	 * @post	If the given name is valid, the name of this new unit is the same as the given name.
+	 * @post	If the given initial position is valid, the initial position of this unit it the same 
+	 * 		as the given initial position.
+	 * @post	If the given weight is in the range of 25 to 100, inclusively, and at least 
+	 * 		(strength+agility)/2, the weight of this new unit is the same as the given weight.
+	 * @post	If the given agility is in the range of 25 to 100, inclusively, the agility of this new
+	 * 		unit is the same as the given agility.
+	 * @post	If the given strength is in the range of 25 to 100, inclusively, the strength of this new
+	 * 		unit is the same as the given strength.
+	 * @post	If the given toughness is in the range of 25 to 100, inclusively, the toughness of this new
+	 * 		unit is the same as the given toughness.
+	 * @post	enableDefaultBehaviour TODO
 	 * @throws 	ModelException
-	 *          A precondition was violated or an exception was thrown.
+	 *      	A precondition was violated or an exception was thrown.
 	 */
 	public Unit(String name, int[] initialPosition, int weight, int agility, int strength, int toughness,
 			boolean enableDefaultBehaviour) throws ModelException{
+		//TODO: implementeren zie initiele grenzen. initial value in the range of 25 to 100, inclusively, and the weight
+		// of a Unit must at all times be at least (s+a)/2
 		setName(name);
 		setCubeCoordinates(initialPosition);
 		setWeight(weight);
@@ -60,15 +80,45 @@ public class Unit {
 	
 	}
 	/**
+	 * Check whether the given name is a valid name for all units.
+	 * @param 	name
+	 * 		The name to check.
+	 * @return	True if and only if the given name is at least two characters long, starts with an uppercase letter and only uses
+	 * 		letters (both uppercase and lowercase), quotes (both single and double) and spaces.
+	 * 		|...TODO
+	 */
+	public static boolean isValidName(String name){
+		if(name.length()>=2 && Character.isUpperCase(name.charAt(0))){
+			for(int i=1;i<name.length();i++){
+				char c = name.charAt(i);
+				if(!(Character.isLetter(c) || validCharContains(c)))
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * TODO
+	 * @param character
+	 * @return
+	 */
+	public static boolean validCharContains(char character){
+		for(int i =0;i<validChars.length;i++){
+			if(validChars[i]==character) return true;
+		}
+		return false;
+	}
+	/**
 	 * Set the name of this unit to the given name.
 	 * 
 	 * @param 	name
-	 * 			The new name of the unit.
+	 * 		The new name of the unit.
 	 * @post	The new name of this unit is equal to the given name.
-	 * 			| new.getName() == name
+	 * 		| new.getName() == name
 	 * @throws 	ModelException
-	 * 			The given name is not valid for any unit.
-	 * 			| ! isValidName(name)
+	 * 		The given name is not valid for any unit.
+	 * 		| ! isValidName(name)
 	 */
 	public void setName(Unit unit, String newname){
 		if (!isValidName(name))
@@ -85,8 +135,8 @@ public class Unit {
 	}
 	/**
 	 * Returns the initial position of the cube occupied by the unit.
-	 * @return ... of @Basic?
 	 */
+	 @Basic
 	public int [] getCubeCoordinate() {
 		int newcoordinates[]= {(int)(Math.floor(this.getPosition()[0])),(int)(Math.floor(this.getPosition()[1])),(int)(Math.floor(this.getPosition()[2]))};
 		return newcoordinates;
@@ -150,9 +200,11 @@ public class Unit {
 		return this.weight;
 	}
 	/**
-	 * 
-	 * @param newweight
-	 * 
+	 * Set the weight of the unit to the given weight.
+	 * @param 	newweight
+	 * 		The new weight of the unit.
+	 * @post	If the given weight is an integer numbers with a value ranging from 1 to 200, inclusively, and at 
+	 * 		least (strength+agility)/2, the new weight of this unit is given by the given weight.
 	 * 
 	 */
 	public void setWeight(int newweight){
@@ -175,7 +227,7 @@ public class Unit {
 	 * @param 	agility
 	 * 		The new agility of the unit.
 	 * @post If the given agility is an integer number with a value less than 0, the new agility of this 
-	 * 	unit is given by 0.
+	 * 	 unit is given by 0.
 	 *       | if(newagility <=0) 
 	 *       |  then  new.getAgility ==1
 	 * @post If the given agility is an integer number with a value greater than 200, the new agility of this 
@@ -195,10 +247,20 @@ public class Unit {
 		else
 			this.agility = newagility;
 	}
+	/**
+	 * Returns the toughness of the unit.
+	 */
 	@Basic
 	public int getToughness(){
 		return this.toughness;
 	}
+	/**
+	 * Set the toughness of the unit to the given toughness.
+	 * @param 	toughness
+	 * 		The new toughness of the unit.
+	 * @post	If the given toughness is an integer numbers with a value ranging from 1 to 200, inclusively, 
+	 * 		the new toughness of this unit is given by the given toughness.
+	 */
 	public void setToughness(int newtoughness){
 		if (newtoughness <=0)
 			this.toughness = 1;
@@ -208,24 +270,83 @@ public class Unit {
 			this.toughness = newtoughness;
 		
 	}
+	/**
+	 * Returns whether the default behaviour is enabled or not.
+	 * 	The default behaviour consists of choosing activities at random. The activity can be (a) move to a random 
+	 * 	position within the game world, (b) conduct a work task, or (c) rest until it has fully recovered hitpoints 
+	 * 	and stamina points. 
+	 */
 	@Basic
-	public double getMaxHitPoints (){
-		return (int)(Math.ceil(200*(this.getWeight()/100.0)*(this.getToughness()/100.0)));
+	public boolean isEnableDefaultBehaviour() {
+		return enableDefaultBehaviour;
+	}
+	/**
+	 * Set the default behaviour of this unit according to the given flag.
+	 * 	
+	 * @param 	enableDefaultBehaviour
+	 * 			The new default behaviour state for this unit.
+	 * @post	The new default behaviour state of this unit is equal to the given flag.
+	 */
+	public void setEnableDefaultBehaviour(boolean enableDefaultBehaviour) {
+		this.enableDefaultBehaviour = enableDefaultBehaviour;
+	}
+	/**
+	 * Start the default behaviour.
+	 * 
+	 * @effect The default behaviour state is set to true.
+	 */
+	public void startDefaultBehaviour(){
 		
 	}
+	/**
+	 * Stop the default behaviour.
+	 * 
+	 * @effect The default behaviour state is set to false.
+	 */
+	public void stopDefaultBehaviour(){
+	/**
+	 * Returns the maximum hit points of the unit.
+	 * TODO @return 200*(weight/100)*(toughness/100) ofzo 
+	 */	
+	}
+	@Basic//Is dit wel zo?
+	public int getMaxHitPoints (){
+		return (int)(Math.ceil(200*(this.getWeight()/100.0)*(this.getToughness()/100.0)));
+	}
+	/**
+	 * Returns the amount of hitpoints of the unit.
+	 * TODO @return ...
+	 */
 	@Basic
 	public double getCurrentHitPoints(){
 		return this.hitpoints;
-		
 	}
+	/**
+	 * Returns the maximum stamina points of the unit.
+	 * TODO @return ...
+	 */
 	@Basic
-	public double getMaxStaminaPoints(){
+	public int getMaxStaminaPoints(){
 		return (int)(Math.ceil(200*(this.getWeight()/100.0)*(this.getToughness()/100.0)));
 	}
+	/**
+	 * Returns the amount of stamina points of the unit.
+	 * TODO @return ...
+	 */
 	@Basic
 	public double getCurrentStaminaPoints(){
 		return this.staminaPoints;
 	}
+	/* Orientation */
+	/**
+	 * Return the current orientation of the unit.
+	 * 
+	 * @param unit
+	 *        The unit for which to retrieve the orientation
+	 * @return The orientation of the unit, in radians.
+	 * @throws ModelException
+	 *          A precondition was violated or an exception was thrown.
+	 */
 	@Basic
 	public double getOrientation(){
 		return this.orientation; 
@@ -248,56 +369,65 @@ public class Unit {
 			neworientation = neworientation + 2*Math.PI;
 		this.orientation = neworientation;
 	}
-	//advance time
-	
-	public void advanceTime(double dt){
+	/*advance time*/
+	/**
+	 * Advance the state of the given unit by the given time period.
+	 * 
+	 * @param 	dt
+	 *          The time period, in seconds, by which to advance the unit's
+	 *          state.
+	 * @post	If the unit is moving, the unit's position and speed gets updated every dt seconds.
+	 * @post	If the unit is sprinting, the unit's position, stamina points and speed gets updated every dt seconds.
+	 * @post	If the unit is working, the unit's position is given and speed is set to zero.
+	 * @post	If the unit is fighting, after one second stop fighting. 			
+	 * @throws 	ModelException
+	 *          A precondition was violated or an exception was thrown.
+	 */
+	public void advanceTime(double dt) throws ModelException{
 		//correcte dt?
-		if !(0<dt<0.2)
-		   throw modelException;
+		if (!(0<dt&& dt<0.2))
+		   throw new ModelException();
 		
 		//snelheid bepalen 
 		if (!(isMoving))
 		    currentSpeed = 0;
 		
-		vb = 1.5 * (this.getStrength()+ this.getAgility()) / (200*this.getWeight()/100);
+		vb = 1.5 * (this.getStrength()+ this.getAgility()) / (200*this.getWeight()/100.0);
 	    
-		if ((this.getPosition()[2]- targetposition[2])==-1)
+		if ((this.getPosition()[2]- targetPosition[2])==-1)
 			vw = 0.5*vb;
-		if ((this.getPosition()[2]- targetposition[2])==1)
+		if ((this.getPosition()[2]- targetPosition[2])==1)
 			vw = 1.2*vb;
 		else 
 			vw = vb;
 		
-		if (this.isSprinting())
-		    currentSpeed = 2*vw ;
+		if (this.isSprinting)
+			currentSpeed = 2*vw ;
 		else
 			currentSpeed = vw;
 		
 		//positie(orientatie)
 		
 		if (isMoving) {
-			distanceToGo = Math.sqrt(Math.pow((targetposition[0]-this.getPosition()[0]),2)+ 
-					       Math.pow((targetposition[1]-this.getPosition()[1]),2)
-			                 +Math.pow((targetposition[2]-this.getPosition()[2]), 2));
-			v = {this.getCurrentSpeed()*(targetposition[0]-this.getPosition()[0])/distanceToGo,
-					this.getCurrentSpeed()*(targetposition[1]-this.getPosition()[1])/distanceToGo,
-					this.getCurrentSpeed()*(targetposition[2]-this.getPosition()[2])/distanceToGo};
+			distanceToGo = Math.sqrt(Math.pow((targetPosition[0]-this.getPosition()[0]),2)+ 
+					       Math.pow((targetPosition[1]-this.getPosition()[1]),2)
+			                 +Math.pow((targetPosition[2]-this.getPosition()[2]), 2));
+			v = new double[]{this.getCurrentSpeed()*(targetPosition[0]-this.getPosition()[0])/distanceToGo,
+					this.getCurrentSpeed()*(targetPosition[1]-this.getPosition()[1])/distanceToGo,
+					this.getCurrentSpeed()*(targetPosition[2]-this.getPosition()[2])/distanceToGo};
 
-		   if (distanceToGo > norm(v)*dt){		
-			
-			this.position = {this.getPosition()[0] + v[0] * dt, this.getPosition()[1]+ v[1]*dt, this.getPosition()[2]+ v[2]*dt};
-			
-			this.setOrientation(Math.atan2(v[0],v[1]));
+			if (distanceToGo > this.getCurrentSpeed()*dt){		
+				this.position = new double[]{this.getPosition()[0] + v[0] * dt, this.getPosition()[1]+ v[1]*dt, this.getPosition()[2]+ v[2]*dt};
+				this.setOrientation(Math.atan2(v[0],v[1]));
 		    }
 		    else 
-		    	this.position = this.targetposition;
+		    	this.position = this.targetPosition;
 		        if (this.endTargetPosition == null)
 		        	this.isMoving = false;
 		    if (this.position == this.endTargetPosition){
 		    	this.isMoving = false;
 		    	this.endTargetPosition = null;
 		    }
-		    
 		}
 	    //Stamina points
 		if (isSprinting)
@@ -365,12 +495,6 @@ public class Unit {
 	
 	//moving
 	
-	public boolean isSprinting;
-	public boolean isMoving;
-	public boolean isWorking;
-	public boolean isFighting;
-	public boolean isResting;
-	public boolean inMinRestTime;	
 		
 		
 	}
